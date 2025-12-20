1d4d0f776ece21f4d955dda8d2411a15d97f801a
Ayush Raj <ayushraj23062004@gmail.com>
2025-12-20 21:09:29 +0530

    Fix binary search loop condition to correctly check final search interval

--- src/practiceQuestions/BinarySearch/findNum.java
+++ src/practiceQuestions/BinarySearch/findNum.java
@@ -28,7 +28,7 @@ public class findNum {
      static int binarySearch(int[]arr,int target){
         int start=0;
         int end=arr.length-1;
-        while(start<end){
+        while(start<=end){
             int mid=start+(end-start)/2;
             if(arr[mid]==target){
                 return mid;
