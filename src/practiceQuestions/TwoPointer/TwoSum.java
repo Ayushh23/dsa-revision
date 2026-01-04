b83e46ab08d0993e406f8ef8d514cf2610683a8a
Ayush Raj <ayushraj23062004@gmail.com>
2026-01-04 23:49:00 +0530

    Used a optimised way for two sum(HashMap)

--- src/practiceQuestions/TwoPointer/TwoSum.java
+++ src/practiceQuestions/TwoPointer/TwoSum.java
@@ -1,12 +1,14 @@
 package practiceQuestions.TwoPointer;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     public static void main(String[] args) {
         int []arr={2,7,11,15};
         int target=9;
-        int []ansarr=twosum(arr,target);
+//        int []ansarr=twosum(arr,target);
+        int[]ansarr=twosumOpt(arr,target);
         System.out.println(Arrays.toString(ansarr));
 
     }
@@ -19,6 +21,21 @@ public class TwoSum {
             }
         }
         return  new int[]{-1,-1};
+
+    }
+
+
+    static int[] twosumOpt(int []arr,int target){
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            int needed=target-arr[i];
+
+            if (map.containsKey(needed)){
+                return new int[]{map.get(needed),i};
+            }
+            map.put(arr[i],i);
+        }
+        return new int[]{-1,-1};
 
     }
 }
