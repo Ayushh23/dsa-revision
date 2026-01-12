413ea858bb96be64fa077f1f33eef78a2d955c47
Ayush Raj <ayushraj23062004@gmail.com>
2026-01-12 23:06:36 +0530

    Solve Move Zeroes Question (Leetcode 283)

--- src/practiceQuestions/Array/moveZeroes.java
+++ src/practiceQuestions/Array/moveZeroes.java
@@ -2,29 +2,21 @@ package practiceQuestions.Array;
 
 import java.util.Arrays;
 
-//Given an integer array nums, move all 0's to the end of it while
-// maintaining the relative order of the non-zero elements.
-//Note that you must do this in-place without making a copy of the array.
-//Example 1:
-//Input: nums = [0,1,0,3,12]
-//Output: [1,3,12,0,0]
 public class moveZeroes {
-    public static  void main(String []ars){
+    public static void main(String[] args) {
         int []arr={0,1,0,3,12};
-       moving(arr);
+        move(arr);
         System.out.println(Arrays.toString(arr));
     }
-    static void moving(int[]arr){
+    static void move(int []arr){
         int k=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0){
-                arr[k]=arr[i];
-                k++;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]!=0){
+                arr[k++]=arr[i];
             }
         }
-        while(k< arr.length){
+        while (k< arr.length){
             arr[k++]=0;
         }
     }
-
 }
